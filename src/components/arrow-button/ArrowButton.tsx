4de9df0bbe02fd08e52fa8d9b1ddc417aa513bad
Karen Petrosian <karenpetrosyan109@yandex.ru>
2024-08-02 04:02:55 +0500

    feat(aside-form): add necessary components

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,32 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import clsx from 'clsx';
 
 /** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface ArrowButtonProps {
+	isSidebarOpen?: boolean;
+	onClick?(): void;
+}
+
+export const ArrowButton = ({ onClick, isSidebarOpen }: ArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
+			onClick={onClick}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(
+				styles.container,
+				isSidebarOpen && styles.container_open
+			)}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, isSidebarOpen && styles.arrow_open)}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,75 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Text } from '../text';
+import { useState } from 'react';
+import clsx from 'clsx';
+import { Select } from '../select';
+import {
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { Space } from '../space/Space';
+import { Separator } from '../separator';
+import { RadioGroup } from '../radio-group';
 
 export const ArticleParamsForm = () => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+	function toggleSidebar() {
+		setIsSidebarOpen((prev) => !prev);
+	}
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton onClick={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+
+			<aside
+				className={clsx(
+					styles.container,
+					isSidebarOpen && styles.container_open
+				)}>
 				<form className={styles.form}>
+					<Text as={'h2'} size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Space />
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={defaultArticleState.fontFamilyOption}
+					/>
+					<Space />
+					<RadioGroup
+						name='size-radio'
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={defaultArticleState.fontSizeOption}
+					/>
+					<Space />
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={defaultArticleState.fontColor}
+					/>
+					<Space />
+					<Separator />
+					<Space />
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={defaultArticleState.backgroundColor}
+					/>
+					<Space />
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={defaultArticleState.contentWidth}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
